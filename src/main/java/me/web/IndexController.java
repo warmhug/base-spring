daca57759e2f4ba921b8bc499da76b28b403cbd6
然则 <hualei.hl@alibaba-inc.com>
2016-01-26 21:40:23 +0800

    autocommit

---
+++ src/main/java/me/web/IndexController.java
@@ -0,0 +1,12 @@
+package me.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    String index() {
+        return "index";
+    }
+}
